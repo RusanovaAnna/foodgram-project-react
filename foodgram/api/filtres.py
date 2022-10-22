5c91206517649ae4a79f6304662145e4e2dd1bf1
Русанова Анна <rusanova.annette@gmail.com>
2022-10-22 13:12:40 +0100

    filter imp

--- foodgram/api/filtres.py
+++ foodgram/api/filtres.py
@@ -1,11 +1,10 @@
-from django_filters import rest_framework as filters
-
+import django_filters
 from recipes.models import Ingredient, Recipe
 from users.models import User
 
 
-class IngredientFilter(filters.FilterSet):
-    name = filters.CharFilter(
+class IngredientFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(
         field_name='name',
         lookup_expr='starts_with'
     )
@@ -15,12 +14,12 @@ class IngredientFilter(filters.FilterSet):
         fields = ('name', 'measurement_unit')
 
 
-class RecipeFilter(filters.FilterSet):
-    name = filters.CharFilter(
+class RecipeFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(
         field_name='name',
     )
-    tags = filters.AllValuesMultipleFilter(field_name='tags__slug',)
-    author = filters.ModelChoiceFilter(queryset=User.objects.all())
+    tags = django_filters.AllValuesMultipleFilter(field_name='tags__slug',)
+    author = django_filters.ModelChoiceFilter(queryset=User.objects.all())
 
     class Meta:
         model = Recipe
